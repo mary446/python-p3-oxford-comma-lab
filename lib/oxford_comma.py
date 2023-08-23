123b172fd7a9ec516e57bc1bb413e68734600476
mary446 <135317707+mary446@users.noreply.github.com>
2023-08-23 11:31:52 +0300

    completed lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
+# convert string into a list use .split()
+# convert a list to a string use .join()
+# oxford.comma ---function designed to format items in the list as gramatically correct and readable sentence
+
 def oxford_comma(items):
-    return None
+    if len(items) == 1:
+        return items[0]
+    # above check if there is one item
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    # above checks if there are two items
+    else:
+        return ', '.join(items[:-1]) + f", and {items[-1]}"
+    # above checks if there are 3 or more items
